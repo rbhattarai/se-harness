267d2da5bdeb35a936904a130018273c0c142d6e
Rohan Bhattarai <rohan.bhattarai@gmail.com>
2026-07-17 23:00:49 -0400

    Removed loan and lending demo apps as it's moved to separate github repo: https://github.com/rbhattarai/demo-loan-app

--- apps/lending-webapp/scripts/copy-assets.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-const projectRoot = path.resolve(__dirname, "..");
-const source = path.join(projectRoot, "src", "views");
-const destination = path.join(projectRoot, "dist", "views");
-
-fs.mkdirSync(destination, { recursive: true });
-fs.cpSync(source, destination, { recursive: true, force: true });
